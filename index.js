cb22723e27d87a65dfeba582e3bb52bfd1155837
Dmytro Babich <70060697+bobbyboy1710@users.noreply.github.com>
2023-01-24 10:49:33 +0200

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(cors());
 
 app.get("/user", (req, res) => {
-  const user = { name: "Alex", age: 40 };
+  const user = { name: "Dmytro Babich", age: 21 };
   res.json({
     response: true,
     user,
